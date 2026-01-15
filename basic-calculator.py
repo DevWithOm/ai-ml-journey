f396919960fd33fa9465a77f7f0847f5589505d1
DevWithOm <vhema0196@gmail.com>
2026-01-15 12:24:33 +0530

    basic calculator added

---
+++ basic-calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter number 1: "))
+num2 = int(input("enter number 2: "))
+add = num1 + num2
+sub = num1-num2
+multiply = num1*num2
+divide = num1/num2
+print("addition is :",add)
+print("subtraction is :",sub)
+print("multiplication is :",multiply)
+print("division is :",divide)
