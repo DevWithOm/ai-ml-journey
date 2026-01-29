0545c8105bdbdfe9d02122be782c4305a3e5055f
DevWithOm <vhema0196@gmail.com>
2026-01-29 13:55:37 +0530

    momentum calculator created

---
+++ python-basics/momentum-calculator.py
@@ -0,0 +1,4 @@
+mass = float(input("Enter the mass of the object (in kg): "))
+velocity = float(input("Enter the velocity of the object (in m/s): "))
+momentum = mass * velocity
+print("The momentum of the object is:", momentum, "kg·m/s")
